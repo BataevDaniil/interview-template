5071a188ea27db6dc56f6b8e8d9acd49c4369747
BataevDaniil <daniil.bataev.iurevich@gmail.com>
2024-08-29 11:30:13 +0300

    add pool for types

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -1,10 +1,10 @@
 import { Global, Module } from '@nestjs/common';
-import { query } from './pg';
+import { query, pool } from './pg';
 import { DB_MODULE_CONNECTION_TOKEN } from './db.constants';
 
 const provider = {
   provide: DB_MODULE_CONNECTION_TOKEN,
-  useValue: { query },
+  useValue: { query, pool },
 };
 @Global()
 @Module({
--- src/db/type.ts
+++ src/db/type.ts
@@ -1,3 +1,14 @@
+type Query = <T extends object>(
+  sql: string,
+  values?: any[],
+) => Promise<{ rows: T[] }>;
+export type Client = {
+  query: Query;
+  release: () => Promise<void>;
+};
 export type DB = {
-  query: (sql: string, values?: any[]) => Promise<{ rows: object[] }>;
+  query: Query;
+  pool: {
+    connect: () => Promise<Client>;
+  };
 };
